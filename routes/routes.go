b8f51eb0335e4fa0fc6da12e755fb909e512eda1
kev1226 <kevin_alt12@hotmail.com>
2025-07-06 17:55:55 -0500

    cors enablement

--- routes/routes.go
+++ routes/routes.go
@@ -2,13 +2,26 @@ package routes
 
 import (
 	"add-service/view"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kev1226/auth-common-go/jwt"
 )
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	// Habilitar CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	cart := r.Group("/cart", jwt.AuthGuard("user"))
 	{
 		cart.POST("", view.AddToCart)
